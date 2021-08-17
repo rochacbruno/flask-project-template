f44b048859d2d135ae2e58f3d0bbe22e23ce9350
Bruno Rocha <rochacbruno@gmail.com>
2021-08-17 17:29:42 +0100

    remove db file

--- project_name/__main__.py
+++ project_name/__main__.py
@@ -3,7 +3,7 @@ from flask.cli import FlaskGroup
 from . import create_app
 
 
-@click.group(cls=FlaskGroup, create_app=create_app)
+@click.group(cls=FlaskGroup, create_app=lambda: create_app())
 def main():
     """Management script for the project_name application."""
 
