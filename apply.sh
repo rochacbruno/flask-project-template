4006d8a0a20bde02812c76d6b302f90400eea3f4
Bruno Rocha <rochacbruno@gmail.com>
2021-08-17 17:07:15 +0100

    Apply script fixed

--- apply.sh
+++ apply.sh
@@ -1,32 +1,60 @@
 #!/usr/bin/env bash
-echo "Creating a flask application"
+while getopts a:n:u:d: flag
+do
+    case "${flag}" in
+        a) author=${OPTARG};;
+        n) name=${OPTARG};;
+        u) urlname=${OPTARG};;
+        d) description=${OPTARG};;
+    esac
+done
+
+echo "Author: $author";
+echo "Project Name: $name";
+echo "Project URL name: $urlname";
+echo "Description: $description";
+
+echo "Rendering the Flask template..."
+original_author="author_name"
+original_name="project_name"
+original_urlname="project_urlname"
+original_description="project_description" 
+TEMPLATE_DIR="./.github/templates/flask"
+for filename in $(find ${TEMPLATE_DIR} -name "*.*" -not \( -name "*.git*" -prune \) -not \( -name "apply.sh" -prune \)) 
+do
+    sed -i "s/$original_author/$author/g" $filename
+    sed -i "s/$original_name/$name/g" $filename
+    sed -i "s/$original_urlname/$urlname/g" $filename
+    sed -i "s/$original_description/$description/g" $filename
+    echo "Renamed $filename"
+done
 
 # Add requirements
 if [ ! -f pyproject.toml ]
 then
-    cat .github/templates/flask/requirements.txt >> requirements.txt
-    cat .github/templates/flask/requirements-test.txt >> requirements-test.txt
+    cat ${TEMPLATE_DIR}/requirements.txt >> requirements.txt
+    cat ${TEMPLATE_DIR}/requirements-test.txt >> requirements-test.txt
 else
-    for item in $(cat .github/templates/flask/requirements.txt)
+    for item in $(cat ${TEMPLATE_DIR}/requirements.txt)
     do
         poetry add "${item}"
     done
-    for item in $(cat .github/templates/flask/requirements-test.txt)
+    for item in $(cat ${TEMPLATE_DIR}/requirements-test.txt)
     do
         poetry add --dev "${item}"
     done
 fi
 
 # Move module files
-rm -rf project_name
+rm -rf "${name}"
 rm -rf tests
-cp -R .github/templates/flask/module project_name
-cp -R .github/templates/flask/tests tests
+cp -R ${TEMPLATE_DIR}/project_name "${name}"
+cp -R ${TEMPLATE_DIR}/tests tests
 
-cp .github/templates/flask/README.md README.md
-cp .github/templates/flask/wsgi.py wsgi.py
-cp .github/templates/flask/.env .env
-cp .github/templates/flask/settings.toml settings.toml
+cp ${TEMPLATE_DIR}/README.md README.md
+cp ${TEMPLATE_DIR}/wsgi.py wsgi.py
+cp ${TEMPLATE_DIR}/.env .env
+cp ${TEMPLATE_DIR}/settings.toml settings.toml
 
 # install
 make clean
@@ -37,10 +65,12 @@ then
     make install
     echo "Applied Flask template"
     echo "Ensure you activate your env with 'source .venv/bin/activate'"
-    echo "then run project_name or python -m project_name"
+    echo "then run 'project_name' or 'python -m project_name'"
 else
     poetry install
     echo "Applied Flask template"
     echo "Ensure you activate your env with 'poetry shell'"
-    echo "then run project_name or python -m project_name or poetry run project_name"
+    echo "then run 'project_name' or 'python -m project_name' or 'poetry run project_name'"
 fi
+
+echo "README.md has instructions on how to use this Flask application."
