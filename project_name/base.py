d04afc2393189d654a0416360064f5d53b522fef
Aviendha Stark Lannister <aviendhasl@digitaldebrief.com>
2023-05-31 05:12:06 +0000

    Pass config to dynaconf.

--- project_name/base.py
+++ project_name/base.py
@@ -4,18 +4,9 @@ from flask import Flask
 
 def create_app(**config):
     app = Flask(__name__)
-    FlaskDynaconf(app)  # config managed by Dynaconf
+    FlaskDynaconf(app, **config)  # config managed by Dynaconf
     app.config.load_extensions(
         "EXTENSIONS"
     )  # Load extensions from settings.toml
     app.config.update(config)  # Override with passed config
-    return app
-
-
-def create_app_wsgi():
-    # workaround for Flask issue
-    # that doesn't allow **config
-    # to be passed to create_app
-    # https://github.com/pallets/flask/issues/4170
-    app = create_app()
     return app
